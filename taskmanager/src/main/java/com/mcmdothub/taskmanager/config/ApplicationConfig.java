db021bc6c7ae78113906e81a780b694636eef5df
cristian <cristian.marin@linearsystem.it>
2025-01-19 21:46:18 +0100

    Configuring JWT Security

--- taskmanager/src/main/java/com/mcmdothub/taskmanager/config/ApplicationConfig.java
+++ taskmanager/src/main/java/com/mcmdothub/taskmanager/config/ApplicationConfig.java
@@ -4,8 +4,12 @@ import com.mcmdothub.taskmanager.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // we need to annotate it with the annotation "@Configuration"
 // means anything inside of this class will be instantiated to my application at startup
@@ -24,5 +28,18 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
