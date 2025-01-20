2d44153bb8f0b29972e01ca9dc919eb23fff74bc
cristian <cristian.marin@linearsystem.it>
2025-01-20 23:12:40 +0100

    Securing Routes with Route Guards

--- taskmanager-frontend/src/app/app.routes.ts
+++ taskmanager-frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import {ProjectComponent} from './project/project.component';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
+import {authGuard} from './auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: ProjectComponent},
+  { path: '', component: ProjectComponent, canActivate: [authGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
